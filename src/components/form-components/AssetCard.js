c45a70b8999823fabc22183a576054b996214bcf
jrzollin <jrzollin@yahoo.com>
2018-02-06 20:38:59 -0800

    added tags display

--- src/components/form-components/AssetCard.js
+++ src/components/form-components/AssetCard.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import { Card, Icon } from 'antd';
+import { Card, Icon, Tag } from 'antd';
 
 require('style-loader!css-loader!antd/es/card/style/index.css');
+require('style-loader!css-loader!antd/es/tag/style/index.css');
 
 const { Meta } = Card;
+
+const labelCheck = (labels) => {
+  console.log(labels);
+  if (!labels) {
+    return null;
+  }
+  if (Object.keys(labels).length === 0) {
+    return null;
+  }
+  return Object.keys(labels).map(label => (<Tag color="#150">{label}</Tag>));
+};
 
 const AssetCard = (props) => {
   const { item } = props;
@@ -12,6 +24,7 @@ const AssetCard = (props) => {
       style={{ width: 300 }}
       cover={<img alt={item.name} src={item.path} />}
       actions={[<Icon type="edit" />]}
+      extra={labelCheck(item.labels)}
     >
       <Meta
         title={item.name}
