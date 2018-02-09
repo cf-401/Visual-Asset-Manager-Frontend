c7ade1248127e8f425a816d01b82af949b2e90ad
meganr <meganrm@gmail.com>
2018-02-08 16:04:42 -0800

    added slack oauth button

--- src/components/log-in/log-in-container.js
+++ src/components/log-in/log-in-container.js
@@ -73,6 +73,7 @@ class LogInContainer extends React.Component {
             />
             <button onClick={this.toggleFormType} name="signin">Log in (existing user)</button>
             <button onClick={this.toggleFormType} name="signup">Sign up (new user)</button>
+            <a href="https://slack.com/oauth/authorize?scope=identity.basic,identity.email,identity.team,identity.avatar&client_id=21630512897.312261919794"><img alt="Sign in with Slack" height="40" width="172" src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcSet="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x" /></a>
           </React.Fragment>,
         )}
 
